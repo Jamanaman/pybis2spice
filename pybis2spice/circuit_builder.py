41b72b046042ec5e313453f30e3aa4bf12b7bc76
Jamanaman <tmdamjanovic@gmail.com>
2026-08-14 10:52:19 +0200

    Comment out diff model generation function for later implementation and update of docstring and function typing to work for Sphinx

--- pybis2spice/circuit_builder.py
+++ pybis2spice/circuit_builder.py
@@ -8,20 +8,28 @@ from pathlib import Path
 def generate_spice_model_file(
         io_type: sckt._IO_TYPE, subcircuit_type:sckt._SIMULATOR, ibis_data: DataModel, 
         corner: sckt._CORNER, truncation: float, output_filepath: str | Path, stimulus: Optional[sckt._STIMULUS] = None 
-        ):
+        ) -> str:
     """
     Wrapper around the subcircuit file creation functions. Calls the relevant function i.e. LTSpice or Generic
 
-        Parameters:
-            io_type - "Input" or "Output" or "Tri-State"\\
-            subcircuit_type - "LTSpice" or "Generic"\\
-            ibis_data - a DataModel object (defined in pybis2spice.py)\\
-            corner - "WeakSlow" or "Typical" or "FastStrong"\\
-            output_filepath - path of output file\\
-            truncation - the percentage of the total range to use to truncate trailing samples in rising and falling waveforms\\
+    Parameters
+    ----------
+        io_type 
+            "Input" or "Output" or "Tri-State"
+        subcircuit_type 
+            "LTSpice" or "Generic"
+        ibis_data 
+            a DataModel object (defined in pybis2spice.py)
+        corner 
+            "WeakSlow" or "Typical" or "FastStrong"
+        output_filepath 
+            path of output file
+        truncation 
+            the percentage of the total range to use to truncate trailing samples in rising and falling waveforms
 
-        Returns:
-            The spice string
+    Returns
+    -------
+        spice string
     """
     try:
         spice_str = ''
@@ -44,44 +52,44 @@ def generate_spice_model_file(
     except Exception as e:
         raise e
 
-def generate_diff_spice_model(
-        io_type: sckt._IO_TYPE, subcircuit_type: sckt._SIMULATOR, ibis_data_p1: DataModel, 
-        ibis_data_p2: DataModel, corner: sckt._CORNER, output_filepath: str, 
-        truncation: int
-        ):
-    try:
-        spice_str = ''
-        if io_type == "Output":
+# def generate_diff_spice_model(
+#         io_type: sckt._IO_TYPE, subcircuit_type: sckt._SIMULATOR, ibis_data_p1: DataModel, 
+#         ibis_data_p2: DataModel, corner: sckt._CORNER, output_filepath: str, 
+#         truncation: int
+#         ):
+#     try:
+#         spice_str = ''
+#         if io_type == "Output":
 
-            if subcircuit_type == "Generic":
-                spice_str = sckt.create_generic_output_model(ibis_data_p1, corner, io_type, truncation)
-                spice_str += sckt.create_generic_output_model(ibis_data_p2, corner, io_type, truncation)
+#             if subcircuit_type == "Generic":
+#                 spice_str = sckt.create_generic_output_model(ibis_data_p1, corner, io_type, truncation)
+#                 spice_str += sckt.create_generic_output_model(ibis_data_p2, corner, io_type, truncation)
 
-            if subcircuit_type == "LTSpice":
-                spice_str = sckt.create_ltspice_output_model(ibis_data_p1, corner, io_type, truncation)
-                spice_str += sckt.create_ltspice_output_model(ibis_data_p2, corner, io_type, truncation)
+#             if subcircuit_type == "LTSpice":
+#                 spice_str = sckt.create_ltspice_output_model(ibis_data_p1, corner, io_type, truncation)
+#                 spice_str += sckt.create_ltspice_output_model(ibis_data_p2, corner, io_type, truncation)
 
-            if subcircuit_type == "ngSPICE":
-                spice_str = sckt.create_ngspice_output_model(ibis_data_p1, corner, io_type, truncation)
-                spice_str += sckt.create_ngspice_output_model(ibis_data_p2, corner, io_type, truncation)
-                spice_str += '.if (stimulus%2==0)\n\n'
-                spice_str += '.param stimulus_inv = stimulus-1\n\n'
-                spice_str += '.else\n\n'
-                spice_str += '.param stimulus_inv = stimulus+1\n\n'
-                spice_str += f'.SUBCKT diff_{ibis_data_p1.model_name}_{ibis_data_p2.model_name}_{corner}_{io_type}'
-                spice_str += 'OUT INV_OUT stimulus=1 freq=10Meg duty=0.5 delay=0 \n\n'
-                spice_str += f'X1 OUT1 {ibis_data_p1.model_name}_{io_type}_{corner}'
-                spice_str += 'stimulus={{stimulus}} freq={{freq}} duty={{duty}} delay={{delay}}\n\n'
-                spice_str += f'X2 INV_OUT {ibis_data_p2.model_name}_{io_type}_{corner}'
-                spice_str += 'stimulus={{stimulus_inv}} freq={{freq}} duty={{duty}} delay={{delay}}\n\n'
-                spice_str += '.ENDS'                
+#             if subcircuit_type == "ngSPICE":
+#                 spice_str = sckt.create_ngspice_output_model(ibis_data_p1, corner, io_type, truncation)
+#                 spice_str += sckt.create_ngspice_output_model(ibis_data_p2, corner, io_type, truncation)
+#                 spice_str += '.if (stimulus%2==0)\n\n'
+#                 spice_str += '.param stimulus_inv = stimulus-1\n\n'
+#                 spice_str += '.else\n\n'
+#                 spice_str += '.param stimulus_inv = stimulus+1\n\n'
+#                 spice_str += f'.SUBCKT diff_{ibis_data_p1.model_name}_{ibis_data_p2.model_name}_{corner}_{io_type}'
+#                 spice_str += 'OUT INV_OUT stimulus=1 freq=10Meg duty=0.5 delay=0 \n\n'
+#                 spice_str += f'X1 OUT1 {ibis_data_p1.model_name}_{io_type}_{corner}'
+#                 spice_str += 'stimulus={{stimulus}} freq={{freq}} duty={{duty}} delay={{delay}}\n\n'
+#                 spice_str += f'X2 INV_OUT {ibis_data_p2.model_name}_{io_type}_{corner}'
+#                 spice_str += 'stimulus={{stimulus_inv}} freq={{freq}} duty={{duty}} delay={{delay}}\n\n'
+#                 spice_str += '.ENDS'                
 
-        if io_type == "Input":
-            spice_str = sckt.create_input_model(ibis_data_p1, corner, io_type, ng=subcircuit_type=="ngSPICE")
-            spice_str += sckt.create_input_model(ibis_data_p2, corner, io_type, ng=subcircuit_type=="ngSPICE")
+#         if io_type == "Input":
+#             spice_str = sckt.create_input_model(ibis_data_p1, corner, io_type, ng=subcircuit_type=="ngSPICE")
+#             spice_str += sckt.create_input_model(ibis_data_p2, corner, io_type, ng=subcircuit_type=="ngSPICE")
         
-        with open(output_filepath, 'w+') as file:
-                file.write(spice_str)
-                return output_filepath
-    except Exception as e:
-        raise e
+#         with open(output_filepath, 'w+') as file:
+#                 file.write(spice_str)
+#                 return output_filepath
+#     except Exception as e:
+#         raise e
